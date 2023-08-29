7d263064d8c6f6a93326ccf2b845be7433a17c01
Gravity cat <29692485+nekogravitycat@users.noreply.github.com>
2023-08-29 23:07:50 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.GET("/", hello)
 	r.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func hello(c *gin.Context) {
+	msg := []byte("hello world")
+	c.Data(http.StatusOK, "text/plain; charset=utf-8", msg)
 }
